dee6498a7cf6c3fd627d2d3c4c18e6a4724aa010
Federico Meloni <federico.meloni@cern.ch>
2023-03-03 06:25:58 -0800

    style

--- plot_occupancy.py
+++ plot_occupancy.py
@@ -1,13 +1,14 @@
 import os
 import logging
-from ROOT import TH1D, TFile, TTree, TColor, TCanvas, TLegend
-from ROOT import kBlack, kBlue, kRed
+from ROOT import TH1D, TFile, TTree, TColor, TCanvas, TLegend, TLatex, TLine
+from ROOT import kBlack, kBlue, kRed, kYellow, kGray
 from ROOT import gStyle, gPad
 from ROOT import gROOT
 from ROOT import TStyle
 from optparse import OptionParser
 import itertools
 from math import fabs
+
 
 def check_output_directory(output_path):
     # checks if output directory exists; if not, mkdir
@@ -28,60 +29,107 @@ fFile = TFile(options.inFile, "READ")
 h_all = fFile.Get('h_nhits')
 h_time = fFile.Get('h_ntimehits')
 
+gStyle.SetPadTopMargin(0.09)
+gStyle.SetPadRightMargin(0.05)
+gStyle.SetPadBottomMargin(0.16)
+gStyle.SetPadLeftMargin(0.15)
 gStyle.SetOptStat(0)
+gStyle.SetPadTickX(1)
+gStyle.SetPadTickY(1)
 
-c1 = TCanvas()
+c1 = TCanvas("", "", 800, 600)
 
-h_all.SetLineColor(kBlack)
-h_all.SetLineWidth(2)
+h_all.SetLineColor(kBlue+1)
+h_all.SetFillColor(kBlue+1)
 h_all.SetTitle("")
-h_all.GetYaxis().SetTitle("Average number of hits / cm^{2}")
-h_all.GetYaxis().SetTitleOffset(1.2)
-#h_all.GetXaxis().SetTitleOffset(1.2)
-#h_all.GetXaxis().SetTitle("")
+h_all.GetYaxis().SetTitle("Average number of hits / cm^{  2}")
+h_all.GetYaxis().SetTitleOffset(1.4)
 h_all.SetMinimum(0.0001)
-h_all.GetXaxis().SetNdivisions(5)
-#h_all.GetXaxis().SetBinLabel(1, "VXD Barrel L1 Inner")
-#h_all.GetXaxis().SetBinLabel(2, "VXD Barrel L1 Outer")
-#h_all.GetXaxis().SetBinLabel(3, "VXD Barrel L2 Inner")
-#h_all.GetXaxis().SetBinLabel(4, "VXD Barrel L2 Outer")
-#h_all.GetXaxis().SetBinLabel(5, "VXD Barrel L3 Inner")
-#h_all.GetXaxis().SetBinLabel(6, "VXD Barrel L3 Outer")
-#h_all.GetXaxis().SetBinLabel(7, "VXD Barrel L4 Inner")
-#h_all.GetXaxis().SetBinLabel(8, "VXD Barrel L4 Outer")
-#h_all.GetXaxis().SetBinLabel(9, "VXD Endcap L1 Side A Inner")
-#h_all.GetXaxis().SetBinLabel(10, "VXD Endcap L1 Side A Outer")
-#h_all.GetXaxis().SetBinLabel(11, "VXD Endcap L2 Side A Inner")
-#h_all.GetXaxis().SetBinLabel(12, "VXD Endcap L2 Side A Outer")
-#h_all.GetXaxis().SetBinLabel(13, "VXD Endcap L1 Side B Inner")
-#h_all.GetXaxis().SetBinLabel(14, "VXD Endcap L1 Side B Outer")
-#h_all.GetXaxis().SetBinLabel(15, "VXD Endcap L2 Side B Inner")
-#h_all.GetXaxis().SetBinLabel(16, "VXD Endcap L2 Side B Outer")
-#h_all.GetXaxis().SetBinLabel(17, "Inner Tracker L1")
+h_all.GetXaxis().SetNdivisions(10)
 h_all.GetXaxis().SetLabelSize(0.04)
+h_all.GetXaxis().SetTitleOffset(1.3)
+h_all.GetXaxis().SetTitle("Tracking Detector Layer")
 h_all.Draw("HIST")
 
-h_time.SetLineColor(kRed)
-h_time.SetLineWidth(2)
+h_time.SetLineColor(kYellow+1)
+h_time.SetFillColor(kYellow+1)
 h_time.Draw("HISTSAME")
 
-h_all.Draw("HISTSAME")
-
 gPad.RedrawAxis()
-gPad.SetTopMargin(0.05)
-gPad.SetBottomMargin(0.15)
-gPad.SetRightMargin(0.15)
-#c1.SetLogy()
 
-leg = TLegend(.45, .62, .77, .83)
+detector_boundaries = [8, 16, 24, 27, 34, 41, 44, 48]
+for bound in detector_boundaries:
+    splitLine = TLine(bound, 0, bound, 600)
+    splitLine.SetLineWidth(2)
+    splitLine.SetLineStyle(2)
+    splitLine.SetLineColor(kGray+1)
+    splitLine.DrawClone("same")
+
+leg = TLegend(.6, .64, .9, .78)
 leg.SetBorderSize(0)
 leg.SetFillColor(0)
 leg.SetFillStyle(0)
 leg.SetTextFont(42)
 leg.SetTextSize(0.035)
-leg.AddEntry(h_all, "All hits", "L")
-leg.AddEntry(h_time, "Time of flight filters", "L")
+leg.AddEntry(h_all, "No time window", "F")
+leg.AddEntry(h_time, "Time window [-3 #sigma_{t}, 5 #sigma_{t}]", "F")
 leg.Draw()
+
+t1 = TLatex()
+t1.SetTextFont(42)
+t1.SetTextColor(1)
+t1.SetTextSize(0.04)
+t1.SetTextAlign(12)
+t1.SetNDC()
+t1.DrawLatex(0.6, 0.85, '#bf{Muon Collider}')
+
+t1_2 = TLatex()
+t1_2.SetTextFont(42)
+t1_2.SetTextColor(1)
+t1_2.SetTextSize(0.04)
+t1_2.SetTextAlign(12)
+t1_2.SetNDC()
+t1_2.DrawLatex(0.6, 0.8, '#it{Simulation}')
+
+t2 = TLatex()
+t2.SetTextFont(42)
+t2.SetTextColor(1)
+t2.SetTextSize(0.035)
+t2.SetTextAlign(12)
+t2.SetNDC()
+t2.DrawLatex(0.25, 0.85, '#sigma_{t}^{VXD}   = 30 ps')
+
+t2_2 = TLatex()
+t2_2.SetTextFont(42)
+t2_2.SetTextColor(1)
+t2_2.SetTextSize(0.035)
+t2_2.SetTextAlign(12)
+t2_2.SetNDC()
+t2_2.DrawLatex(0.25, 0.79, '#sigma_{t}^{IT, OT} = 60 ps')
+
+t2_3 = TLatex()
+t2_3.SetTextFont(42)
+t2_3.SetTextColor(1)
+t2_3.SetTextSize(0.035)
+t2_3.SetTextAlign(12)
+t2_3.SetNDC()
+t2_3.DrawLatex(0.25, 0.73, 'B_{solenoid} = 5 T')
+
+t3 = TLatex()
+t3.SetTextFont(42)
+t3.SetTextColor(1)
+t3.SetTextSize(0.035)
+t3.SetTextAlign(12)
+t3.SetNDC()
+t3.DrawLatex(0.15, 0.94, 'Background hits overlay in [-360, 480] ps range')
+
+t4 = TLatex()
+t4.SetTextFont(42)
+t4.SetTextColor(1)
+t4.SetTextSize(0.035)
+t4.SetTextAlign(12)
+t4.SetNDC()
+t4.DrawLatex(0.82, 0.94, '#sqrt{s} = 10 TeV')
 
 c1.SaveAs("TrackerOccupancy.pdf")
 
